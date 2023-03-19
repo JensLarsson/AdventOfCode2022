12ea36121983db117b7329e0f91c028033b96dfa
Jens Larsson <Larssonjenz@gmail.com>
2023-03-19 23:28:15 +0100

    feat: Add solutions for Day1
    Solve Day1 using Linq

---
+++ src/Day1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System.Linq;
+
+
+if (args.Length == 0)
+{
+    Console.WriteLine("Usage: provide input file in form of txt file as arg");
+    return;
+}
+
+string inputPath = args[0];
+string input = File.ReadAllText(inputPath);
+
+var containers = input.Split("\n\n")
+    .Select(line => line.Split('\n', StringSplitOptions.RemoveEmptyEntries))
+    .Select(container =>
+    container.Select(item => int.TryParse(item, out int parsedVal) ? parsedVal : 0).Sum()).ToList();
+
+containers.Sort();
+Console.WriteLine(containers[^1] + containers[^2] + containers[^3]);
