820d700b47f49a4e181f95e5431308eeaa31e275
Jens Larsson <Larssonjenz@gmail.com>
2023-04-06 00:28:26 +0200

    refactor: Replace direct console writing with a StringBuilder

--- src/Day10/Program.cs
+++ src/Day10/Program.cs
@@ -1,6 +1,7 @@
-﻿class Program
+﻿using System.Text;
+
+class Program
 {
-    static char[] outputString = new char[40];
     static void Main(string[] args)
     {
         if (args.Length == 0)
@@ -8,53 +9,48 @@
             Console.WriteLine("Usage: provide input file in form of txt file as arg");
             return;
         }
-        string[] lines = File.ReadAllLines(args[0]);
+        var lines = File.ReadAllLines(args[0]);
 
         int registerValue = 1;
         int instructionCount = 0;
+
         int totalSignalValue = 0;
+        StringBuilder output = new StringBuilder();
 
         foreach (string line in lines)
         {
             string[] instructions = line.Split(' ');
-            switch (instructions[0])
+            //noop no operation or first operation of addx
+            AddCharToOutputString(output, registerValue, instructionCount);
+            instructionCount++;             //use instruction for operation
+            totalSignalValue += GetSignalValue(registerValue, instructionCount);
+            if (instructions[0] == "addx")  //add x to register
             {
-                case "noop":        //no operation
-                    instructionCount++;
-                    totalSignalValue += HandleInstructionTick(registerValue, instructionCount);
-                    PrintOutputString(registerValue, instructionCount);
-                    break;
-                case "addx":        //add x to register
-                    instructionCount++;
-                    totalSignalValue += HandleInstructionTick(registerValue, instructionCount);
-                    PrintOutputString(registerValue, instructionCount);
-                    instructionCount++;
-                    totalSignalValue += HandleInstructionTick(registerValue, instructionCount);
-                    registerValue += int.Parse(instructions[1]);
-                    PrintOutputString(registerValue, instructionCount);
-                    break;
+                AddCharToOutputString(output, registerValue, instructionCount);
+                instructionCount++;         //use another instruction for addx operation
+                totalSignalValue += GetSignalValue(registerValue, instructionCount);
+                registerValue += int.Parse(instructions[1]);
             }
         }
+        Console.WriteLine(output.ToString());
         Console.WriteLine(totalSignalValue);
     }
 
-    static void PrintOutputString(int registerValue, int instructionCount)
+    static void AddCharToOutputString(StringBuilder output, int registerValue, int instructionCount)
     {
         int i = instructionCount % 40;
-        outputString[i] = (i >= registerValue - 1 && i <= registerValue + 1) ? '#' : '.';
-
-        if (instructionCount % 40 == 0)
+        if (i == 0)
         {
-            Console.WriteLine(outputString);
+            output.Append('\n');
         }
+        output.Append((i >= registerValue - 1 && i <= registerValue + 1) ? '#' : '.');
     }
 
     /// <summary>
     /// Returns the signal value if on a designated instruction(20, or every 40 after that), otherwise return 0
     /// </summary>
-    static int HandleInstructionTick(int registerValue, int instructionCount)
+    static int GetSignalValue(int registerValue, int instructionCount)
     {
-
         if ((instructionCount - 20) % 40 == 0)
         {
             return registerValue * instructionCount;
