05ebd6ba05db1aa63aa40d9fa5cf13b75d366cfc
Jens Larsson <Larssonjenz@gmail.com>
2023-04-09 23:54:42 +0200

    refactor: Speedup refactor

--- src/Day11/Program.cs
+++ src/Day11/Program.cs
@@ -1,13 +1,14 @@
-﻿using System.Numerics;
+﻿using System.Diagnostics;
+using System.Numerics;
 
 class Monkey : IComparable<Monkey>
 {
     public List<BigInteger> Items { get; set; } = new List<BigInteger>();
     public Func<BigInteger, BigInteger> Operation { get; init; }
-    public ulong TestInt { get; init; }
+    public int TestInt { get; init; }
     public int OnTrueTarget { get; init; }
     public int OnFalseTarget { get; init; }
-    public ulong ItemsInspected { get; set; } = 0;
+    public int ItemsInspected { get; set; } = 0;
 
     public int CompareTo(Monkey? other)
     {
@@ -38,39 +39,46 @@ class Program
             monkeys.Add(new Monkey
             {
                 Items = input[i + 1].Split(new char[] { ',', ' ' }, StringSplitOptions.RemoveEmptyEntries)
-                .Select(x => BigInteger.TryParse(x, out BigInteger y) ? y : (BigInteger?)null) //parse to ulong or null
-                .Where(x => x.HasValue)                                         //filter out nulls
-                .Select(x => x.Value).ToList(),                                 //unwrap optionals
+                .Where(x => BigInteger.TryParse(x, out _))
+                .Select(x => BigInteger.Parse(x))
+                .ToList(),
 
                 Operation = GetOperation(input[i + 2]),
-                TestInt = ulong.Parse(input[i + 3].Split(' ')[^1]),
+                TestInt = int.Parse(input[i + 3].Split(' ')[^1]),
                 OnTrueTarget = int.Parse(input[i + 4].Split(' ')[^1]),
                 OnFalseTarget = int.Parse(input[i + 5].Split(' ')[^1])
             });
         }
 
+        Stopwatch sw = new Stopwatch();
+        sw.Start();
+
         for (int round = 0; round < rounds; round++)
         {
             foreach (var monkey in monkeys)
             {
-                monkey.ItemsInspected += (ulong)monkey.Items.Count;
-                Parallel.ForEach(monkey.Items, item =>
+                monkey.ItemsInspected += monkey.Items.Count;
+
+                for (int i = 0; i < monkey.Items.Count; i++)
                 {
-                    BigInteger newItemScore = monkey.Operation(item) / 3; //divide by 3 and round down to whole number
-                    if (newItemScore % monkey.TestInt == 0)
+                    BigInteger item = monkey.Items[i];
+                    BigInteger newItemScore = monkey.Operation(item); // divide by 3 and round down to whole number
+                    if ((newItemScore % monkey.TestInt) == 0)
                     {
-                        lock (monkeys[monkey.OnTrueTarget])
-                            monkeys[monkey.OnTrueTarget].Items.Add(newItemScore);
+                        monkeys[monkey.OnTrueTarget].Items.Add(newItemScore);
                     }
                     else
                     {
-                        lock (monkeys[monkey.OnFalseTarget])
-                            monkeys[monkey.OnFalseTarget].Items.Add(newItemScore);
+                        monkeys[monkey.OnFalseTarget].Items.Add(newItemScore);
                     }
-                });
-                monkey.Items = new List<BigInteger>();
+                }
+                // Clear the items list
+                monkey.Items.Clear();
             }
         }
+        sw.Stop();
+        Console.WriteLine($"Time elapsed: {sw.ElapsedMilliseconds} ms");
+
         foreach (var monkey in monkeys)
         {
             Console.WriteLine($"Monkey {monkeys.IndexOf(monkey)} inspected {monkey.ItemsInspected} items");
@@ -79,13 +87,15 @@ class Program
         Console.WriteLine($"monkey business after {rounds} rounds: {monkeys[^1].ItemsInspected * monkeys[^2].ItemsInspected}");
     }
 
+
     static Func<BigInteger, BigInteger> GetOperation(string instruction)
     {
         var instructions = instruction.Split(' ');
+        bool useParsedVal = BigInteger.TryParse(instructions[^1], out BigInteger y);
         return instructions[^2] switch
         {
-            "+" => x => x + (BigInteger.TryParse(instructions[^1], out BigInteger y) ? y : x),    //assume that failed parse means use old value
-            "*" => x => x * (BigInteger.TryParse(instructions[^1], out BigInteger y) ? y : x),    //assume that failed parse means use old value
+            "+" => x => x + (useParsedVal ? y : x),    //assume that failed parse means use old value
+            "*" => x => x * (useParsedVal ? y : x),    //assume that failed parse means use old value
             _ => throw new ArgumentException("Invalid instruction")
         };
     }
