39a231cc57ae197a0c1c146367d2a444dafb40ed
Jens Larsson <Larssonjenz@gmail.com>
2023-04-10 23:12:05 +0200

    perf: Use Modular arithmetic, and remove BigInteger top make Part2 executable
    Part1 still broken

--- src/Day11/Program.cs
+++ src/Day11/Program.cs
@@ -1,14 +1,39 @@
 ﻿using System.Diagnostics;
 using System.Numerics;
 
+class Item
+{
+    public List<int> ValuesPerMonkey { get; init; }
+    public List<int> Divisors { get; init; }
+    public Item(List<int> divisors, int startValue)
+    {
+        ValuesPerMonkey = new List<int>();
+        Divisors = divisors;
+        foreach (int divisor in divisors)
+        {
+            ValuesPerMonkey.Add(startValue % divisor);
+        }
+    }
+    public void RunOperator(Func<int, int> operation)
+    {
+        for (int i = 0; i < ValuesPerMonkey.Count; i++)
+        {
+            ValuesPerMonkey[i] = operation(ValuesPerMonkey[i]) % Divisors.ElementAt(i);
+        }
+    }
+}
+
 class Monkey : IComparable<Monkey>
 {
-    public List<BigInteger> Items { get; set; } = new List<BigInteger>();
-    public Func<BigInteger, BigInteger> Operation { get; init; }
-    public int TestInt { get; init; }
+    public List<Item> Items { get; set; } = new List<Item>();
+    public Func<int, int> Operation { get; init; }
+    public int Index { get; init; }
+    public int Divisor { get; init; }
     public int OnTrueTarget { get; init; }
     public int OnFalseTarget { get; init; }
-    public int ItemsInspected { get; set; } = 0;
+    public ulong ItemsInspected { get; set; } = 0;
+
+
 
     public int CompareTo(Monkey? other)
     {
@@ -34,21 +59,28 @@ class Program
         int rounds = int.Parse(args[1]);
 
         List<Monkey> monkeys = new List<Monkey>();
+        List<List<int>> itemValues = new List<List<int>>();
         for (int i = 0; i < input.Length; i += 7)
         {
+            itemValues.Add(input[i + 1].Split(new char[] { ',', ' ' }, StringSplitOptions.RemoveEmptyEntries)
+                .Where(x => int.TryParse(x, out _))
+                .Select(x => int.Parse(x))
+                .ToList());
             monkeys.Add(new Monkey
             {
-                Items = input[i + 1].Split(new char[] { ',', ' ' }, StringSplitOptions.RemoveEmptyEntries)
-                .Where(x => BigInteger.TryParse(x, out _))
-                .Select(x => BigInteger.Parse(x))
-                .ToList(),
-
+                Index = int.Parse(input[i].Split(new char[] { ':', ' ' }, StringSplitOptions.RemoveEmptyEntries)[^1]),
                 Operation = GetOperation(input[i + 2]),
-                TestInt = int.Parse(input[i + 3].Split(' ')[^1]),
+                Divisor = int.Parse(input[i + 3].Split(' ')[^1]),
                 OnTrueTarget = int.Parse(input[i + 4].Split(' ')[^1]),
                 OnFalseTarget = int.Parse(input[i + 5].Split(' ')[^1])
             });
         }
+        List<int> divisors = monkeys.Select(x => x.Divisor).ToList();
+        for (int i = 0; i < monkeys.Count; i++)
+        {   //create items for each monkey
+            monkeys[i].Items = itemValues[i].Select(x => new Item(divisors, x)).ToList();
+        }
+
 
         Stopwatch sw = new Stopwatch();
         sw.Start();
@@ -57,19 +89,19 @@ class Program
         {
             foreach (var monkey in monkeys)
             {
-                monkey.ItemsInspected += monkey.Items.Count;
+                monkey.ItemsInspected += (ulong)monkey.Items.Count;
 
                 for (int i = 0; i < monkey.Items.Count; i++)
                 {
-                    BigInteger item = monkey.Items[i];
-                    BigInteger newItemScore = monkey.Operation(item); // divide by 3 and round down to whole number
-                    if ((newItemScore % monkey.TestInt) == 0)
+                    monkey.Items[i].RunOperator(monkey.Operation);
+                    int newItemScore = monkey.Items[i].ValuesPerMonkey[monkey.Index];
+                    if ((newItemScore % monkey.Divisor) == 0)
                     {
-                        monkeys[monkey.OnTrueTarget].Items.Add(newItemScore);
+                        monkeys[monkey.OnTrueTarget].Items.Add(monkey.Items[i]);
                     }
                     else
                     {
-                        monkeys[monkey.OnFalseTarget].Items.Add(newItemScore);
+                        monkeys[monkey.OnFalseTarget].Items.Add(monkey.Items[i]);
                     }
                 }
                 // Clear the items list
@@ -88,10 +120,10 @@ class Program
     }
 
 
-    static Func<BigInteger, BigInteger> GetOperation(string instruction)
+    static Func<int, int> GetOperation(string instruction)
     {
         var instructions = instruction.Split(' ');
-        bool useParsedVal = BigInteger.TryParse(instructions[^1], out BigInteger parseOut);
+        bool useParsedVal = int.TryParse(instructions[^1], out int parseOut);
         return instructions[^2] switch
         {
             "+" => x => x + (useParsedVal ? parseOut : x),    //assume that failed parse means use old value
