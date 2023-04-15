763d53b079642d5a61156a081c2cd417caca6305
Jens Larsson <Larssonjenz@gmail.com>
2023-04-15 23:49:48 +0200

    feat: Add Boilerplate for Day13

---
+++ src/Day13/Program.cs
@@ -0,0 +1,13 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        if (args.Length != 1)
+        {
+            Console.WriteLine("Usage: dotnet run <input file>");
+            return;
+        }
+        var input = File.ReadAllLines(args[0]);
+
+    }
+}
