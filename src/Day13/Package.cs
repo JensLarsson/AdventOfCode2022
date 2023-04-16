a1e6e51af56d8facade050aadd825d21461e4104
Jens Larsson <Larssonjenz@gmail.com>
2023-04-16 23:01:25 +0200

    refactor: Convert to Object Oriented solution with DotNet IComparable implementation

---
+++ src/Day13/Package.cs
@@ -0,0 +1,48 @@
+using System.Text.Json;
+class Package : IComparable<Package>
+{
+    public JsonDocument Signal { get; init; }
+
+    public Package(string signal)
+    {
+        this.Signal = JsonDocument.Parse(signal);
+    }
+
+    public int CompareTo(Package? other)
+    {
+        int leftLenght = Signal.RootElement.GetArrayLength();
+        int rightLenght = other.Signal.RootElement.GetArrayLength();
+        int shortestLength = Math.Min(leftLenght, rightLenght);
+        for (int i = 0; i < shortestLength; i++)
+        {
+            JsonElement leftProperty = Signal.RootElement[i];
+            JsonElement rightProperty = other.Signal.RootElement[i];
+
+            // If either property is an array, we need to compare the arrays
+            if (leftProperty.ValueKind == JsonValueKind.Array || rightProperty.ValueKind == JsonValueKind.Array)
+            {
+                bool leftIsNumber = leftProperty.ValueKind == JsonValueKind.Number;
+                bool IsRightNumber = rightProperty.ValueKind == JsonValueKind.Number;
+                Package left = new Package(leftIsNumber ? "[" + leftProperty.ToString() + "]" : leftProperty.ToString());
+                Package right = new Package(IsRightNumber ? "[" + rightProperty.ToString() + "]" : rightProperty.ToString());
+
+                int result = left.CompareTo(right);
+                if (result == 0)
+                {
+                    continue;
+                }
+                return result;
+            }
+
+            // If both properties are numbers, we can compare the numbers
+            int compare = leftProperty.GetInt32().CompareTo(rightProperty.GetInt32());
+            if (compare == 0)
+            {
+                continue;
+            }
+            return compare;
+        }
+        // If we get here, we have compared all the properties and they are all equal
+        return leftLenght.CompareTo(rightLenght);
+    }
+}
--- src/Day13/Program.cs
+++ src/Day13/Program.cs
@@ -1,6 +1,4 @@
-﻿using System.Text.Json;
-
-class Program
+﻿class Program
 {
     static void Main(string[] args)
     {
@@ -18,61 +16,12 @@ class Program
     {
         for (int i = 0; i < input.Length; i += 3)
         {
-            var left = JsonDocument.Parse(input[i]);
-            var right = JsonDocument.Parse(input[i + 1]);
-            if (IsRightOrder(left, right) != Order.Wrong) //treat undecided as correct
+            var left = new Package(input[i]);
+            var right = new Package(input[i + 1]);
+            if (left.CompareTo(right) <= 0)
             {
                 yield return (i + 3) / 3;
             }
         }
-    }
-
-    enum Order
-    {
-        Correct, Wrong, Undecided
-    }
-    static Order IsRightOrder(JsonDocument leftList, JsonDocument rightList)
-    {
-        int leftLenght = leftList.RootElement.GetArrayLength();
-        int rightLenght = rightList.RootElement.GetArrayLength();
-        int shortestLength = Math.Min(leftLenght, rightLenght);
-        for (int i = 0; i < shortestLength; i++)
-        {
-            JsonElement leftProperty = leftList.RootElement[i];
-            JsonElement rightProperty = rightList.RootElement[i];
-
-            // If either property is an array, we need to compare the arrays
-            if (leftProperty.ValueKind == JsonValueKind.Array || rightProperty.ValueKind == JsonValueKind.Array)
-            {
-                bool leftIsNumber = leftProperty.ValueKind == JsonValueKind.Number;
-                bool IsRightNumber = rightProperty.ValueKind == JsonValueKind.Number;
-                JsonDocument letfArray = JsonDocument.Parse(leftIsNumber ? "[" + leftProperty.ToString() + "]" : leftProperty.ToString());
-                JsonDocument rightArray = JsonDocument.Parse(IsRightNumber ? "[" + rightProperty.ToString() + "]" : rightProperty.ToString());
-                Order result = IsRightOrder(letfArray, rightArray);
-                if (result == Order.Undecided)
-                {
-                    continue;
-                }
-                return result;
-            }
-
-            // If both properties are numbers, we can compare the numbers
-            switch (leftProperty.GetInt32().CompareTo(rightProperty.GetInt32()))
-            {
-                case -1:
-                    return Order.Correct;
-                case 1:
-                    return Order.Wrong;
-                default:
-                    continue;
-            }
-        }
-        // If we get here, we have compared all the properties and they are all equal
-        return (leftLenght.CompareTo(rightLenght)) switch
-        {
-            -1 => Order.Correct,
-            1 => Order.Wrong,
-            _ => Order.Undecided
-        };
     }
 }
