e9b57df965a6856de8ecbd850da7af614b36098f
Jens Larsson <Larssonjenz@gmail.com>
2023-05-01 00:13:19 +0200

    feat: Place and write out edges of lines

--- src/Day14/Program.cs
+++ src/Day14/Program.cs
@@ -13,6 +13,11 @@
         X = int.Parse(XY[0]);
         Y = int.Parse(XY[1]);
     }
+
+    public override string ToString()
+    {
+        return $"({X}:{Y})";
+    }
 }
 
 class Program
@@ -30,6 +35,8 @@ class Program
             .Select(s => s.Split(" -> ")
                 .Select(xyString => new Vector2(xyString)));
 
+        Console.WriteLine(lines.Count());
+
         Vector2 botLeft = new Vector2(int.MaxValue, int.MaxValue);
         Vector2 topRight = new Vector2(0, 0);
 
@@ -37,20 +44,46 @@ class Program
         {
             foreach (Vector2 point in line)
             {
+                Console.WriteLine(point.ToString());
                 botLeft.X = Math.Min(point.X, botLeft.X);
                 botLeft.Y = Math.Min(point.Y, botLeft.Y);
                 topRight.X = Math.Max(point.X, topRight.X);
                 topRight.Y = Math.Max(point.Y, topRight.Y);
             }
+            Console.WriteLine("-");
         }
+        Console.WriteLine(botLeft.ToString());
+        Console.WriteLine(topRight.ToString());
 
-        bool[,] map = new bool[topRight.X - botLeft.X, topRight.Y - botLeft.Y];
+        bool[,] map = new bool[topRight.X - botLeft.X + 1, topRight.Y - botLeft.Y + 1];
+        int rows = map.GetLength(1);
+        int cols = map.GetLength(0);
+        Console.WriteLine(rows);
+        Console.WriteLine(cols);
         foreach (IEnumerable<Vector2> line in lines)
         {
             foreach (Vector2 point in line)
             {
-                map[point.X, point.Y] = true;
+                map[point.X - botLeft.X, point.Y - botLeft.Y] = true;
             }
         }
+
+
+        for (int i = 0; i < rows; i++)
+        {
+            for (int j = 0; j < cols; j++)
+            {
+                if (map[j, i])
+                {
+                    Console.Write('#');
+                }
+                else
+                {
+                    Console.Write('.');
+                }
+            }
+            Console.WriteLine();
+        }
+
     }
 }
