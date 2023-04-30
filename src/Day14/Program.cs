f8215b692c30ae3baa3266b3918cb45e01ec1b43
Jens Larsson <Larssonjenz@gmail.com>
2023-04-30 23:57:40 +0200

    feat: Add Boilerplate for Day14

---
+++ src/Day14/Program.cs
@@ -0,0 +1,56 @@
+﻿struct Vector2
+{
+    public int X { get; set; }
+    public int Y { get; set; }
+    public Vector2(int x, int y)
+    {
+        X = x;
+        Y = y;
+    }
+    public Vector2(string vectorString)
+    {
+        var XY = vectorString.Split(',');
+        X = int.Parse(XY[0]);
+        Y = int.Parse(XY[1]);
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        if (args.Length != 1)
+        {
+            Console.WriteLine("Usage: provide input file in form of txt file as arg");
+            return;
+        }
+
+        string[] input = File.ReadAllLines(args[0]);
+        IEnumerable<IEnumerable<Vector2>> lines = input
+            .Select(s => s.Split(" -> ")
+                .Select(xyString => new Vector2(xyString)));
+
+        Vector2 botLeft = new Vector2(int.MaxValue, int.MaxValue);
+        Vector2 topRight = new Vector2(0, 0);
+
+        foreach (IEnumerable<Vector2> line in lines)
+        {
+            foreach (Vector2 point in line)
+            {
+                botLeft.X = Math.Min(point.X, botLeft.X);
+                botLeft.Y = Math.Min(point.Y, botLeft.Y);
+                topRight.X = Math.Max(point.X, topRight.X);
+                topRight.Y = Math.Max(point.Y, topRight.Y);
+            }
+        }
+
+        bool[,] map = new bool[topRight.X - botLeft.X, topRight.Y - botLeft.Y];
+        foreach (IEnumerable<Vector2> line in lines)
+        {
+            foreach (Vector2 point in line)
+            {
+                map[point.X, point.Y] = true;
+            }
+        }
+    }
+}
